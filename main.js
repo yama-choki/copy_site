92b7593c814c9ab00784d4d05465627596270a8f
yama-choki <lovepingpong0@gmail.com>
2021-05-25 12:04:54 +0900

    サイトのカスタマイズ

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// {
+
+//   const title = document.getElementById('title');
+
+//   function showTime() {
+//    title.classList.add('show');  
+//   }
+//   setInterval(showTime, 1000); 
+// }
